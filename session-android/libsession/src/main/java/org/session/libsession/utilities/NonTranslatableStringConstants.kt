6088ab85466c1778a35b0327483354ea2487d061
mpretty-cyro <15862619+mpretty-cyro@users.noreply.github.com>
2024-08-21 00:13:42 +0000

    [Automated] Update translations from Crowdin

---
+++ session-android/libsession/src/main/java/org/session/libsession/utilities/NonTranslatableStringConstants.kt
@@ -0,0 +1,13 @@
+package org.session.libsession.utilities
+
+// Non-translatable strings for use with the UI
+object NonTranslatableStringConstants {
+    const val TOKEN_NAME_SHORT       = "SENT"
+    const val TOKEN_NAME_LONG        = "Session Token"
+    const val TOKEN_NAME_LONG_PLURAL = "Session Tokens"
+    const val ETHEREUM               = "Ethereum"
+    const val NETWORK_NAME           = "Session Network"
+    const val APP_NAME               = "Session"
+    const val ARBITRUM               = "Arbitrum"
+    const val ARBISCAN               = "Arbiscan"
+}
